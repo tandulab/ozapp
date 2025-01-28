35705c209796a75c6d26cc8b8087c3a42ce3d3de
Sara <sarabertaina@hotmail.it>
2025-01-28 14:49:46 +0100

    first build

---
+++ assets/index-DM0mS0M8.js
@@ -0,0 +1,74 @@
+import{aW as r,B as u,p as o,o as d,a as p}from"./index-BMmkjg9s.js";var l={name:"BaseInput",extends:r,props:{size:{type:String,default:null},fluid:{type:Boolean,default:null},variant:{type:String,default:null}},inject:{$parentInstance:{default:void 0},$pcFluid:{default:void 0}},computed:{$variant:function(){var n;return(n=this.variant)!==null&&n!==void 0?n:this.$primevue.config.inputStyle||this.$primevue.config.inputVariant},$fluid:function(){var n;return(n=this.fluid)!==null&&n!==void 0?n:!!this.$pcFluid},hasFluid:function(){return this.$fluid}}},c=function(n){var t=n.dt;return`
+.p-inputtext {
+    font-family: inherit;
+    font-feature-settings: inherit;
+    font-size: 1rem;
+    color: `.concat(t("inputtext.color"),`;
+    background: `).concat(t("inputtext.background"),`;
+    padding-block: `).concat(t("inputtext.padding.y"),`;
+    padding-inline: `).concat(t("inputtext.padding.x"),`;
+    border: 1px solid `).concat(t("inputtext.border.color"),`;
+    transition: background `).concat(t("inputtext.transition.duration"),", color ").concat(t("inputtext.transition.duration"),", border-color ").concat(t("inputtext.transition.duration"),", outline-color ").concat(t("inputtext.transition.duration"),", box-shadow ").concat(t("inputtext.transition.duration"),`;
+    appearance: none;
+    border-radius: `).concat(t("inputtext.border.radius"),`;
+    outline-color: transparent;
+    box-shadow: `).concat(t("inputtext.shadow"),`;
+}
+
+.p-inputtext:enabled:hover {
+    border-color: `).concat(t("inputtext.hover.border.color"),`;
+}
+
+.p-inputtext:enabled:focus {
+    border-color: `).concat(t("inputtext.focus.border.color"),`;
+    box-shadow: `).concat(t("inputtext.focus.ring.shadow"),`;
+    outline: `).concat(t("inputtext.focus.ring.width")," ").concat(t("inputtext.focus.ring.style")," ").concat(t("inputtext.focus.ring.color"),`;
+    outline-offset: `).concat(t("inputtext.focus.ring.offset"),`;
+}
+
+.p-inputtext.p-invalid {
+    border-color: `).concat(t("inputtext.invalid.border.color"),`;
+}
+
+.p-inputtext.p-variant-filled {
+    background: `).concat(t("inputtext.filled.background"),`;
+}
+
+.p-inputtext.p-variant-filled:enabled:hover {
+    background: `).concat(t("inputtext.filled.hover.background"),`;
+}
+
+.p-inputtext.p-variant-filled:enabled:focus {
+    background: `).concat(t("inputtext.filled.focus.background"),`;
+}
+
+.p-inputtext:disabled {
+    opacity: 1;
+    background: `).concat(t("inputtext.disabled.background"),`;
+    color: `).concat(t("inputtext.disabled.color"),`;
+}
+
+.p-inputtext::placeholder {
+    color: `).concat(t("inputtext.placeholder.color"),`;
+}
+
+.p-inputtext.p-invalid::placeholder {
+    color: `).concat(t("inputtext.invalid.placeholder.color"),`;
+}
+
+.p-inputtext-sm {
+    font-size: `).concat(t("inputtext.sm.font.size"),`;
+    padding-block: `).concat(t("inputtext.sm.padding.y"),`;
+    padding-inline: `).concat(t("inputtext.sm.padding.x"),`;
+}
+
+.p-inputtext-lg {
+    font-size: `).concat(t("inputtext.lg.font.size"),`;
+    padding-block: `).concat(t("inputtext.lg.padding.y"),`;
+    padding-inline: `).concat(t("inputtext.lg.padding.x"),`;
+}
+
+.p-inputtext-fluid {
+    width: 100%;
+}
+`)},s={root:function(n){var t=n.instance,a=n.props;return["p-inputtext p-component",{"p-filled":t.$filled,"p-inputtext-sm p-inputfield-sm":a.size==="small","p-inputtext-lg p-inputfield-lg":a.size==="large","p-invalid":t.$invalid,"p-variant-filled":t.$variant==="filled","p-inputtext-fluid":t.$fluid}]}},f=u.extend({name:"inputtext",theme:c,classes:s}),x={name:"BaseInputText",extends:l,style:f,provide:function(){return{$pcInputText:this,$parentInstance:this}}},g={name:"InputText",extends:x,inheritAttrs:!1,methods:{onInput:function(n){this.writeValue(n.target.value,n)}},computed:{attrs:function(){return o(this.ptmi("root",{context:{filled:this.$filled,disabled:this.disabled}}),this.formField)}}},v=["value","disabled","aria-invalid"];function b(i,n,t,a,h,e){return d(),p("input",o({type:"text",class:i.cx("root"),value:i.d_value,disabled:i.disabled,"aria-invalid":i.$invalid||void 0,onInput:n[0]||(n[0]=function(){return e.onInput&&e.onInput.apply(e,arguments)})},e.attrs),null,16,v)}g.render=b;export{l as a,g as s};
