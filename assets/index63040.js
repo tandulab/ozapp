ebb8c38be9a66292990961d14f3af89c054e2479
GiacomoDamosso <g.damosso@tandu.it>
2025-12-17 10:47:18 +0100

    build

--- assets/index63040.js
+++ assets/index63040.js
@@ -1,4 +1,4 @@
-import{B as f,Q as m,x as $,R as v,i as y,o as s,c as p,a as d,L as o,m as a,t as B,C as c,n as k,f as g,w as h,e as w,U as P,q as C,H as D,af as A,s as S}from"../index31082.js";import{s as L,a as I}from"./index310822.js";var K=({dt:e})=>`
+import{B as f,Q as m,x as $,R as v,i as y,o as s,c as p,a as d,L as o,m as a,t as B,C as c,n as k,f as g,w as h,e as w,U as P,q as C,H as D,af as A,s as S}from"../index63040.js";import{s as L,a as I}from"./index630402.js";var K=({dt:e})=>`
 .p-panel {
     border: 1px solid ${e("panel.border.color")};
     border-radius: ${e("panel.border.radius")};
