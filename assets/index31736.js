767445bc61b82e12233add2b55a4e162272cdf2b
GiacomoDamosso <g.damosso@tandu.it>
2026-02-09 11:21:06 +0100

    build

--- assets/index31736.js
+++ assets/index31736.js
@@ -1,4 +1,4 @@
-import{B as f,Q as m,x as $,R as v,i as y,o as s,c as p,a as d,L as o,m as a,t as B,C as c,n as k,f as g,w as h,e as w,U as P,q as C,H as D,ae as A,s as S}from"../index60953.js";import{s as L,a as I}from"./index609532.js";var K=({dt:e})=>`
+import{B as f,Q as m,x as $,R as v,i as y,o as s,c as p,a as d,L as o,m as a,t as B,C as c,n as k,f as g,w as h,e as w,U as P,q as C,H as D,ae as A,s as S}from"../index31736.js";import{s as L,a as I}from"./index317362.js";var K=({dt:e})=>`
 .p-panel {
     border: 1px solid ${e("panel.border.color")};
     border-radius: ${e("panel.border.radius")};
